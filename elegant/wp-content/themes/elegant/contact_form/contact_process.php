b3aab5c8fcc01686ac955cc464d68d79c52f0697
KlavierCat <plin@tcd.ie>
2014-12-12 14:41:08 +0000

    change e-mail in contact form

--- elegant/wp-content/themes/elegant/contact_form/contact_process.php
+++ elegant/wp-content/themes/elegant/contact_form/contact_process.php
@@ -8,18 +8,18 @@ if(empty($_POST['name'])  		||
 	echo "Please fill in all three fields!";
 	return false;
    }
-	
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
+
 // Create the email and send the message
-$to = 'plin@tcd.ie'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@elegantirishtours.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@panpanlin.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers = "From: noreply@elegantirishtours.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 echo "Your message is sent!";
-return true;			
+return true;
 ?>
